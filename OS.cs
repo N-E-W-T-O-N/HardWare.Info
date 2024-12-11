5f48147eed1f0da445222260e04ea44cd4bfb9fc
NEWTON MALLICK <NEW99MAL@GMAIL.COM>
2024-12-11 11:40:33 +0000

    Added GPU Test

---
+++ OS.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using Hardware.Info;
+
+class OS
+{
+    public static bool IsWindows() =>
+        RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    public static bool IsMacOS() =>
+        RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+    public static bool IsLinux() =>
+        RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+    public static bool IsGpu() => GetGpuDetails();
+
+    /// <summary>
+    /// Check For
+    /// </summary>
+    /// <returns></returns>
+    private static bool GetGpuDetails()
+    {
+        List<string> approved = ["NVIDIA"];
+
+        IHardwareInfo hardwareInfo = new HardwareInfo();
+
+        hardwareInfo.RefreshVideoControllerList();
+
+        bool result = false;
+        foreach (var h in hardwareInfo.VideoControllerList)
+        {
+            result = approved.Any(x => x.Contains(h.Description, StringComparison.InvariantCultureIgnoreCase));
+            if (result) break;
+        }
+
+        return result;
+
+    }
+}
